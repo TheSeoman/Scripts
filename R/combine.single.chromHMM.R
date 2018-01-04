fbfee261b45ff0a489804d95ff7cc0681d6beffb
ICB Julian Schmidt <julian.schmidt@heidi.scidom.de>
2018-01-04 15:00:19 +0100

    combine

--- R/combine.single.chromHMM.R
+++ R/combine.single.chromHMM.R
@@ -25,52 +25,50 @@ get.ranges.from.annotation <- function (annotation) {
 
 combine.single.1nt.chromHMM <-
   function (sample.dir, samples, ranges, start = 1, end = 27) {
-    annotation.list <- lapply(list.files(sample.dir)[c(start:end)], function(file) {
-        message(paste0('loading: ', file))
-        load(paste0(sample.dir, file))
-        return(annotation)
-      })
-    annotation.ranges.list <-
-      get.ranges.from.annotation(annotation.list)
+    annotation.list <- lapply(list.files(sample.dir)[start:end], function(file) {
+      message(paste0('loading: ', file))
+      load(paste0(sample.dir, file))
+      temp <- unlist(lapply(annotation, function (ann) ann[[1]][4]))
+      return(temp)
+    })
+    ranges.map <- names(ranges)  
+    names(ranges.map) <- paste0(seqnames(ranges), ':', start(ranges), '-', end(ranges))
     
     samples <- samples[order(samples)]
     
+    ann.list <- lapply(annotation.list, function(annotation) {
+      annotation.loc <- names(annotation)
+      missing.loc <- ranges.map[!(names(ranges.map) %in% annotation.loc)]
+      names(annotation) <- ranges.map[annotation.loc]
+      annotation[missing.loc] <- NA
+      annotation <- annotation[order(names(annotation))]
+      return(annotation)
+    })
+    
     annotation.combined <-
       data.frame(matrix(nrow = length(ranges), ncol = length(annotation.list)))
-    rownames(annotation.combined) <- names(ranges)
-    colnames(annotation.combined) <- samples[c(start:end)]
-    prev.id <- ''
-    for (i in c(1:length(annotation.list))) {
-      message(paste0('Processing: ', samples[i]))
-      overlap <-
-        findOverlaps(ranges, annotation.ranges.list[[i]])
-      for (j in c(1:length(overlap))) {
-        if (j %% round(length(overlap) / 1000) == 0) {
-          message(paste0(samples[i], ': ', round(j / length(overlap) * 1000) / 10, '%'))
-        }
-        colnames(annotation.combined) <- samples
-        for (i in c(1:length(annotation.list))) {
-          overlap <- findOverlaps(ranges, annotation.ranges.list[[i]])
-          for (j in c(1:length(overlap))) {
-            id <- names(ranges)[queryHits(overlap)[j]]
-            if (id != prev.id) {
-              annotation.combined[id, samples[i]] <-
-                annotation.ranges.list[[i]][subjectHits(overlap)[j]]$state
-              prev.id <- id
-            }
-          }
-        }
-      }
+    
+    rownames(annotation.combined) <- names(ranges)[order(names(ranges))]
+    colnames(annotation.combined) <- samples[start:end]
+    
+    for (i in 1:length(ann.list)) {
+      annotation.combined[, i] <- ann.list[[i]]
     }
+    
     return(annotation.combined)
   }
 
-# run
-# args <- commandArgs()
 load(PATHS$CHROMHMM.SAMPLE.DATA)
-load(PATHS$METH.RANGES.DATA)
-sample.dir <- paste0(PATHS$DATA.DIR, 'chromHMM/meth.ranges/')
+# meth
+# load(PATHS$METH.RANGES.DATA)
+# sample.dir <- paste0(PATHS$DATA.DIR, 'chromHMM/meth.ranges/')
+# meth.chromhmm.states <- combine.single.1nt.chromHMM(sample.dir, ids, meth.ranges)
+# save(meth.chromhmm.states, file = PATHS$METH.CHROMHMM.DATA)
 
-meth.chromhmm.states <- combine.single.1nt.chromHMM(sample.dir, ids, meth.ranges)
+# snp
+load(PATHS$SNP.RANGES.DATA)
+sample.dir <- paste0(PATHS$DATA.DIR, 'chromHMM/snp.ranges/')
+snp.chromhmm.states <- combine.single.1nt.chromHMM(sample.dir, ids, snp.ranges)
+save(snp.chromhmm.states, file = PATHS$SNP.CHROMHMM.DATA)
 
-save(meth.chromhmm.states, file = PATHS$METH.CHROMHMM.DATA)
+
